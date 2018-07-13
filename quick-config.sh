f00982dda3817f42511cb61e3137b4a41bc3c99d
Tim Stahel <swedneck@protonmail.ch>
2018-07-13 12:50:49 +0200

    add bash hashbang

--- quick-config.sh
+++ quick-config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://localhost:3000"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
